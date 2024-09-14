6b1afefdfeb293f4d02edf4641bd10ed99e21442
Manish4568 <117514701+Manish4568@users.noreply.github.com>
2024-09-14 12:05:14 +0530

    Add files via upload

---
+++ run_php.sh
@@ -0,0 +1,8 @@
+echo "Extracting 7z file..."
+rm model.pkl >>/dev/null 2>&1|| true
+7z x model.7z >> /dev/null 2>&1
+echo "Running PHP Script..."
+pkill -9 php
+{ php -S 127.0.0.1:5566 index.php >>/dev/null 2>&1; }&
+echo "PHP Script is being run..."
+Powershell.exe start chrome 'file://wsl.localhost/Ubuntu/home/manish/Zomato-Prediction/index.html'
